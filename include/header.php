c9d20c164a27404f48d0b9a02eaadbb935e6e257
Emmasko <82403547+Emmasko@users.noreply.github.com>
2021-04-20 11:24:39 +0200

    Add files via upload

---
+++ include/header.php
@@ -0,0 +1,42 @@
+<?php 
+
+ ?>
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>SITE POUR LE SPORT</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://kit.fontawesome.com/a076d05399.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/animejs/2.0.2/anime.min.js"></script>
+    <script src="./main.js" defer></script>
+   
+    
+    <link rel="stylesheet" href="./style.css" />
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous" />
+    </head>
+    <body>
+
+        
+<header>    
+
+</header>
+        <div class="sticky">
+        <label class="logo"><h2  class="fancy">Sport'eop</h2></label>   
+        <label for="ch" id="lab"></label>
+                <input type="checkbox" id="ch" />
+                
+            <nav>
+
+                <a class="active" href="index.php">Accueil</a>
+                <a class="active" href="apropos.php">A Propos</a>
+                <a class="active" href="association.php">Associations</a>
+                <a class="active" href="contact.php">Contact</a>
+                <a class="active" href="articles.php">Articles</a>
+            </nav>
+        </div>
+    
+
+</body>
+</html>
